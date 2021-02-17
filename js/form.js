fa4d4023fe856331757867ceb234d92b6df4c21d
namanmp <73427423+namanmp@users.noreply.github.com>
2021-02-17 18:20:08 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,30 @@
+class Form{
+    constructor(){
+
+    }
+display(){
+    var title=createElement("h2")
+    title.html("Car Racing Game")
+    title.position(130,0)
+
+    var input=createInput("name")
+    var button=createButton("play")
+    input.position(130,160)
+    button.position(250,200)
+    button.mousePressed(function(){
+        input.hide()
+        button.hide()
+        var name=input.value();
+        playerCount+=1
+        player.update(name)
+        player.updateCount(playerCount)
+        var greetings=createElement("h3")
+        greetings.html("hello" +" " +name)
+        greetings.position(130,160)
+
+    })
+}
+
+
+
+}
